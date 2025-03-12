85bfb0d5fde2d39a1848ff1e38f44f28c735ccc7
JustinTingthealpaca <tingk4@kgv.hk>
2025-03-12 12:59:21 +0800

    Create script.js

---
+++ calculator/script.js
@@ -0,0 +1,16 @@
+let input1 = 0;
+let input2 = 0;
+let mode = 0;
+
+function compute(number1, number2) {
+  if (mode = 0) {
+    number1 + number2 = number1;
+  } else if(mode = 1) {
+    number1 - number2 = number1;
+  } else if(mode = 2) {
+    number1 * number2 = number1;
+  } else if(mode = 3) {
+    number1 / number2 = number1;
+  }
+  return number1;
+}
