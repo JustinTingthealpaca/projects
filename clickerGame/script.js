7e75a34e0dbc498f70c0410e2e92cc3a20af322a
JustinTingthealpaca <tingk4@kgv.hk>
2025-06-03 10:07:10 +0800

    v0.1 clicker
    innitial update

--- clickerGame/script.js
+++ clickerGame/script.js
@@ -1,11 +1,42 @@
 const display = document.getElementById("display");
-let number = 0;
+const button1Page = document.getElementById("button1Page");
+const costAmount1 = document.getElementById("costAmount1");
+const increaseButtonValueBy1Button = document.getElementById("increaseButtonValueBy1Button");
+let money = 0;
+let amount = 1;
 
-function add(x) {
-    number += x;
+//upgrade work
+let increaseButtonValueBy1 = {
+    displayName: "+1 CLICK",
+    cost: 10,
+    opeartion: "Increases Money per Click by 1!"
+}
+
+
+function button(x) {
+    if (x == 0) {
+        if (increaseButtonValueBy1.cost <= money) {
+            money -= increaseButtonValueBy1.cost;
+            increaseButtonValueBy1.cost = increaseButtonValueBy1.cost * 1.5;
+            amount++;
+        }
+    }
+
+    log();
+}
+
+function add() {
+    money += amount;
     log();
 }
 
 function log() {
-    display.innerHTML = number;
+    display.innerHTML = Math.floor(money);
+    updateShop();
+}
+
+//update shop
+function updateShop() {
+    //first button
+    costAmount1.innerHTML = Math.floor(increaseButtonValueBy1.cost);
 }
